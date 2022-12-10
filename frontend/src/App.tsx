5ee61180edf1bfb26ea583bb60df9dbe9ce9380a
Cory Simpson <cory.simpson.dev@gmail.com>
2022-12-09 22:04:25 -0800

    initialized react app with typescript and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Div100vh from 'react-div-100vh'
+
+function App() {
+  return (
+    <Div100vh className="bg-black">
+      <h1 className="text-3xl font-bold text-white underline">
+        Hello world!
+      </h1>
+    </Div100vh>
+  );
+}
+
+export default App;
