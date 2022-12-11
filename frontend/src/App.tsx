7c2fa2ae35631e2b38556b8aacd37ce3c4b65cbf
Frencezz Hernandez <cory.simpson.dev@gmail.com>
2022-12-10 22:24:12 -0800

    added rules page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Div100vh from 'react-div-100vh'
+import RulesModal from './components/RulesModal'
+
+// add logic/state for rules in local storage (add opaque background)
 
 function App() {
   return (
-    <Div100vh className="bg-black">
-      <h1 className="text-3xl font-bold text-white underline">
-        Hello world!
-      </h1>
+    <Div100vh className="bg-black max-w-md mx-auto flex flex-col">
+      <RulesModal />
     </Div100vh>
   );
 }
---
+++ frontend/src/components/RulesModal.tsx
@@ -0,0 +1,19 @@
+function RulesModal() {
+  return (
+    <div className="w-full h-full p-10 bg-white/25 z-11 relative">
+        <div className="absolute top-5 right-5 w-4 h-4 rounded-lg bg-white/50 flex flex-col justify-center text-center">
+            X
+        </div>
+        <div className="flex flex-col justify-center rounded-xl bg-white h-full">
+            <div className="mx-auto">
+              Howdy
+            </div>
+            <div className="mt-10 mx-auto">
+              Bum
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RulesModal
