96a7062dddf83408e5b4f4e933d9846a9afa6687
Cory Simpson <cory.simpson.dev@gmail.com>
2022-12-14 23:51:13 -0800

    added MediumButton & Teams button components

---
+++ frontend/src/components/buttons/MediumButton.tsx
@@ -0,0 +1,15 @@
+// NEXT ROUND
+// END GAME
+
+function MediumButton(text: string) {
+  return (
+    <button
+        type="button"
+        className="flex justify-center items-center rounded-md border border-gray-300 bg-white px-6 py-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
+      >
+        {text}
+    </button>
+  )
+}
+
+export default MediumButton
---
+++ frontend/src/components/buttons/Teams.tsx
@@ -0,0 +1,15 @@
+// Team One
+// Team Two
+
+function Teams(text: string) {
+  return (
+    <button
+        type="button"
+        className="flex justify-center items-center rounded-md border border-gray-300 bg-white px-2 py-1 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50"
+      >
+        {text}
+    </button>
+  )
+}
+
+export default Teams
